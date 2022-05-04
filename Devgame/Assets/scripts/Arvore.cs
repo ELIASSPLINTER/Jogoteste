d48da78bd05d19cb08b7ac953e9d1a7618bd6376
ELIAS COELHO RIBEIRO <62073430+ELIASSPLINTER@users.noreply.github.com>
2022-05-04 10:59:17 -0300

    m

---
+++ Devgame/Assets/scripts/Arvore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arvore : MonoBehaviour
+{
+
+    [SerializeField] private int healht ;
+
+    private Dropitens Drops;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Drops = GetComponent<Dropitens>();
+    }
+
+    private void OnMouseDown() {
+        Debug.Log("entrou mause");
+         TakeDamage(1);
+    }
+
+    private void TakeDamage(int damage){
+        healht -= damage;
+
+        if(healht <= 0){
+            Debug.Log("entrou if");
+            OnEnemyDied();
+        }
+             
+
+    }
+
+    private void OnEnemyDied(){
+
+        Drops.Drop();
+
+        Destroy(gameObject);
+        Debug.Log("entrou destroi");
+    }
+
+
+}
---
+++ Devgame/Assets/scripts/Dropitens.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropitens : MonoBehaviour
+{
+
+    [SerializeField] private GameObject item;
+    [SerializeField] private int itemDropRate;
+    [SerializeField] private int itemMinDrop;
+    [SerializeField] private int itemMaxDrop;
+
+    public void Drop(){
+        int rand = Random.Range(1,100);
+
+        print(rand);
+
+        if(rand< itemDropRate){
+
+            int amount = Random.Range(itemMinDrop,itemMaxDrop);
+
+            for(int i = 0;i< amount;i++){
+                
+                Instantiate(item,transform.position,transform.rotation);
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
